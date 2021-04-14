a24f848ea13dcb30d237667e0c73f82ebf5c03b5
kiruthika <kiruthika.kd.99@gmail.com>
2021-04-14 13:21:27 +0530

    Updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,5 +12,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.print("Hi!This is HARSHAVARDHAN");
 	}
 }
